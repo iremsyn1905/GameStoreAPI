29316f9cfc8eb30d52a26785529cef381ec2a9f4
iremsyn1905 <iremsyn1905@gmail.com>
2026-07-02 16:15:35 +0300

    Add files via upload

---
+++ WebApplication1/DTOs/CreateGameDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStoreAPI.DTOs
+{
+    public class CreateGameDto
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public bool IsInstalled { get; set; }
+
+    }
+}
---
+++ WebApplication1/GameItem.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStoreAPI
+{
+    public class GameItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public bool IsInstalled { get; set; }
+
+    }
+}
